f5f207ad3fda531f4a6d69e0762e2ff0ab4fa79b
Akinpennu Taiwo <moriyebas@gmail.com>
2023-12-11 12:48:42 +0300

    firstcommit

---
+++ nsu_project/urls.py
@@ -0,0 +1,47 @@
+"""
+URL configuration for nsu_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include,re_path
+from nsuapp import views
+from rest_framework.urlpatterns import  format_suffix_patterns
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+
+
+
+
+urlpatterns = [
+    
+    path('admin/', admin.site.urls),
+    path('info/', (views.info_list)),
+    path('info/<int:id>', (views.info_detail)),
+    re_path(r'^favicon\.ico$', favicon_view)
+   
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# urlpatterns=format_suffix_patterns(urlpatterns)
+
+
+
