00f5938f5739692f6b85860e5f3d099388c15368
Akinpennu Taiwo <moriyebas@gmail.com>
2023-12-14 16:39:07 +0300

    taiwo

--- nsu_project/settings.py
+++ nsu_project/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # STATIC_URL = 'static/'
@@ -32,7 +34,7 @@ MEDIA_URL = '/media/'
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0hd@37u&px3^(@cugjq(uyeuwsigfr+p)u3cqg4v6=k)jn&cof'
+SECRET_KEY = env("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -123,11 +125,11 @@ WSGI_APPLICATION = 'nsu_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':"django.db.backends.postgresql",
-        'NAME':"test",
-        'USER':"postgres",
-        'PASSWORD':"3310",
-        'HOST':"localhost",
-        'PORT':"5432",
+        'NAME':env("DB_NAME"),
+        'USER':env("DB_USER"),
+        'PASSWORD':env("DB_PASSWORD"),
+        'HOST':env("DB_HOST"),
+        'PORT':env("DB_PORT"),
     }
 }
 
