f093389c90fed792db0e0dec939ec45518ba828b
Akinpennu Taiwo <moriyebas@gmail.com>
2023-12-14 16:14:29 +0300

    Taiwo

--- nsu_project/settings.py
+++ nsu_project/settings.py
@@ -120,16 +120,16 @@ WSGI_APPLICATION = 'nsu_project.wsgi.application'
 #     # }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE':"django.db.backends.postgresql",
-#         'NAME':"test",
-#         'USER':"postgres",
-#         'PASSWORD':"3310",
-#         'HOST':"localhost",
-#         'PORT':"5432",
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE':"django.db.backends.postgresql",
+        'NAME':"test",
+        'USER':"postgres",
+        'PASSWORD':"3310",
+        'HOST':"localhost",
+        'PORT':"5432",
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -192,21 +192,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'Your Email'
-# EMAIL_HOST_PASSWORD = 'Your Password'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
 
 
-AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID '
-AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY'
-AWS_STORAGE_BUCKET_NAME = 'AWS_STORAGE_BUCKET_NAME'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'AWS_S3_REGION_NAME'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
