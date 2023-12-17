0a5b49dd9a8382e70686318833da24f88973b607
Akinpennu Taiwo <moriyebas@gmail.com>
2023-12-17 07:09:39 +0300

    k

--- nsu_project/settings.py
+++ nsu_project/settings.py
@@ -124,7 +124,7 @@ WSGI_APPLICATION = 'nsu_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':"django.db.backends.postgresql",
+        'ENGINE':"django.db.backends.postgresql_psycopg2",
         'NAME':"yf",
         'USER':"postgres",
         'PASSWORD':"3310",
